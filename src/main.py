5f1eecd155e4440074f276fa92fc6940ea93143d
albert <ruben_2015@mail.ru>
2024-03-14 10:38:39 +0800

    add lvl

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import utils
 
 
-# данные карты
+# Стандартизирую данные карты
 
 data_cart = utils.get_list()
 
@@ -20,23 +20,23 @@ for data_operation in reversed(data_cart):
 for data_operation in data_cart:
     # Удаляю "Т" в дате
     data_operation['date'] = data_operation['date'].replace('T',' ')
-    # Оставляю только дату и время
-    data_operation['date'] = data_operation['date'][:19]
+    # Оставляю только дату
+    data_operation['date'] = data_operation['date'][:10]
 
 # Получаем последние 5 операций
 last_five_operations_cart = utils.get_last_five_operations(data_cart)
 # форматирую дату
-last_five_operations_cart = utils.formation_date(last_five_operations_cart)
-# добаляем "Отсутствует" если from отсутствует
+last_five_operations_cart = utils.format_date(last_five_operations_cart)
+# Если from отсутствует то добаляем "Отсутствует"
 last_five_operations_cart = utils.add_from_in_operations(last_five_operations_cart)
 # Маскеруем счет отправителя
 last_five_operations_cart = utils.disguise_sender_account(last_five_operations_cart)
 # Маскеруем счет получателя
 last_five_operations_cart = utils.disguise_recipients_account(last_five_operations_cart)
 
-# Выводим 5 последних операций
+# Выводим 5 последних операци
 for operations in reversed(last_five_operations_cart):
-    print(operations['date'], end=' ')
+    print(operations["date"], end=' ')
     print(operations["description"])
     print(operations["from"], end=' -> ')
     print(operations["to"])
